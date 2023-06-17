873722482d414843eabbc11f4882106e45978dec
gustavomgs <53432968+gustavomgs@users.noreply.github.com>
2023-06-17 15:49:55 -0300

    Add files via upload

---
+++ Media.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp.senai.br.calculomedias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class Media {
+    
+    public static void main(String args[]){   
+       
+        Calculos biblioteca = new Calculos();
+        
+    	System.exit(0);
+		
+    }
+}
